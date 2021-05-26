64c2d9bc1e9bd3e37a0bab2487a3c11ff63b7546
Lukasz Byszewski <lukasz.byszewski@gmail.com>
2021-05-26 17:03:04 +0200

    second Changes

--- src/main/java/JavaStart/newTaxes/CalculateTax.java
+++ src/main/java/JavaStart/newTaxes/CalculateTax.java
@@ -3,6 +3,8 @@ package JavaStart.newTaxes;
 public class CalculateTax {
 
     private  double salary;
+    private  double wifeSlaary;
+    private  double husbandSlaary;
 
     public double getSalary() {
         return salary;
@@ -13,16 +15,49 @@ public class CalculateTax {
     }
 
 
+    public double getWifeSlaary() {
+        return wifeSlaary;
+    }
+
+    public void setWifeSlaary(double wifeSlaary) {
+        this.wifeSlaary = wifeSlaary;
+    }
+
+    public double getHusbandSlaary() {
+        return husbandSlaary;
+    }
+
+    public void setHusbandSlaary(double husbandSlaary) {
+        this.husbandSlaary = husbandSlaary;
+    }
+
     public double calculateSalary(double salary){
+        double standardPrice = 2500;
         double result = 0;
-        if (salary < 2500){
+        if (salary <= standardPrice){
              result = salary * 9/100;
-            return result;
+             double finalResultation = salary - result;
+            return finalResultation;
         } else {
-            double result2 = ((salary - 2500) * 17/100 ) - result;
-            double finalResult = salary - result2;
+            double result2 = (salary * 9/100) - ((salary - standardPrice) * 17/100 )   ;
+            double finalResult = salary - result2 ;
             return finalResult;
         }
 
     }
+    public double newCalculation (double wifeSlaary, double husbandSlaary){
+        double finalNewResult = 0;
+
+        if(wifeSlaary <= husbandSlaary){
+
+            finalNewResult = (husbandSlaary) - ((husbandSlaary * 80/100)) ;
+            double x = finalNewResult * 8/100;
+            double w =  finalNewResult - x;
+            double y =  wifeSlaary + w;
+            return y ;
+        } else {
+            return 0;
+        }
+    }
+
 }
